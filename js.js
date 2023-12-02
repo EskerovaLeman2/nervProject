a41ae93da7b726bc1928f0a3974f736a8150d402
leman <sipsirin@leman.com>
2023-12-02 03:39:03 -0800

    Project1

--- js.js
+++ js.js
@@ -8,9 +8,30 @@ const personalMovieDB = {
     privat: false
 }
 
-let answer1 = prompt('Axirinci baxdiginiz film?');
-let answer2 = prompt('Bu kinonu neche bal ile qiymetlendirisiniz?');
+for (let i = 0; i < numberOfFilms; i++) {
 
-personalMovieDB.movies[answer1] = answer2;
+    let answer1 = prompt('Axirinci baxdiginiz film?');
+    let answer2 = prompt('Bu kinonu neche bal ile qiymetlendirisiniz?');
+
+    if (answer1 == '' || answer1 == null || answer2 == '' || answer2 == null || answer1.length > 50) {
+        console.log("Sehv bash verdi");
+        i--;
+    } else {
+        personalMovieDB.movies[answer1] = answer2;
+    }
+}
+
+if (personalMovieDB.count < 10) {
+    console.log('Cox az filme baxmisiniz');
+} else if (personalMovieDB.count < 30) {
+    console.log("Klassik Film izleyicisi");
+} else if (personalMovieDB.count > 30) {
+    console.log('Siz Kinomansiniz');
+} else {
+    console.log("Sehv bas verdi")
+}
+
+
+
 
 console.log(personalMovieDB);
